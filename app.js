66a09fef53ae6af182cdf0fac206dd158ec38329
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2019-08-05 20:59:56 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+// Connect MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+  .then(res => console.log('MongoDB Connected!'));
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello'));
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
